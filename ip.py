6f1176bf080ef4e980783021ec685b66dfb9dd67
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-08-30 15:56:32 +0330

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,28 +1,16 @@
 
 import requests
-import socket
+from fake_useragent import UserAgent
 
-def send_request(url, method="GET"):
-    if method == "POST":
-        data = {"key": "value"}  # اطلاعات برای بدنه درخواست POST را تهیه کنید
-        response = requests.post(url, data=data)
-    elif method == "GET":
-        response = requests.get(url)
-    elif method == "SYN":
-        ip = socket.gethostbyname(url)
-        print("Ip AddReSs ", ip)
-        return
-    elif method == "UAM":
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}  # یک User-Agent مناسب تهیه کنید
-        response = requests.get(url, headers=headers)
+url = input(" Enter Url: ")
 
-    print(response.text)  # پاسخ سرور را بررسی کنید
+user_agent = UserAgent()
 
-# تعداد ریکوئست را از کاربر دریافت می کنیم
-num_requests = int(input(" Enter count of Requests (def=10000): "))
-url = input(" Enter target url or ip: ")
-method = input(" Enter the method (POST/GET/SYN/UAM): ")
-
-# ریکوئست ها را ارسال می کنیم
-for i in range(num_requests):
-    send_request(url, method)
+for _ in range(5000):
+    headers = {
+        'User-Agent': user_agent.random
+    }
+    response = requests.get(url, headers=headers)
+    # اینجا می‌توانید کد دلخواه خود را قرار دهید
+    
+    print(f'Response status code: {response.status_code}')
