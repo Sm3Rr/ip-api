8a789cfc367ee8339a15ca95cd7e7eab1f8f165e
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-08-30 14:51:52 +0330

    Update ip.py

--- ip.py
+++ ip.py
@@ -2,7 +2,7 @@
 import requests
 
 # URL مورد نظر خود را در اینجا قرار دهید
-url = "http://www.example.com"
+url = input("http://www.example.com")
 
 # User-Agent جعلی که میخواهید برای ریکوئست استفاده شود
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
