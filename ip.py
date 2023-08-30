36b31b07d9326020dde635722aa03735f5261b87
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-08-30 16:02:21 +0330

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,16 +1,22 @@
 
+
 import requests
-from fake_useragent import UserAgent
+import random
 
-url = input(" Enter Url: ")
+def send_requests(url, num_requests):
+    for _ in range(num_requests):
+        headers = {'User-Agent': generate_user_agent()}
+        requests.get(url, headers=headers)
 
-user_agent = UserAgent()
+def generate_user_agent():
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.3',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.3',
+    ]
+    return random.choice(user_agents)
 
-for _ in range(5000):
-    headers = {
-        'User-Agent': user_agent.random
-    }
-    response = requests.get(url, headers=headers)
-    # اینجا می‌توانید کد دلخواه خود را قرار دهید
-    
-    print(f'Response status code: {response.status_code}')
+URL = input(" Enter Url: ")
+NUM_REQUESTS = int(input(" Enter Req's Amount: "))
+
+send_requests(URL, NUM_REQUESTS)
